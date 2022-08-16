13d967d3069fba72993e044bdc2721e39d75b53c
tillzmh <tillzmh@users.noreply.github.com>
2022-08-16 12:28:59 -0400

    create const thatll be used throughout the app

---
+++ index.js
@@ -0,0 +1,22 @@
+const Employee = require
+const Manger = require
+const Engineer = require
+const Intern = require
+const inquirer = require ("inquirer")
+const fs = require('fs')
+const generteHtml = require 
+const empquestions = [
+    {
+        message: "ID?",
+        name: "id",
+    },
+    {
+        message: "Name?",
+        name: "name",
+    },
+    {
+        message: "Email?",
+        name: "email",
+    }
+    
+]
