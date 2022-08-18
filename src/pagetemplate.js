e21400bb459343232d739908231effd8e2fa6131
tillzmh <tillzmh@users.noreply.github.com>
2022-08-18 17:04:57 -0400

    created page template with css style

---
+++ src/pagetemplate.js
@@ -0,0 +1,52 @@
+module.exports = function (team){
+    var html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <title>TeamCity</title>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <style>
+body {
+        margin: 0
+        padding: 0
+        display: inline-block;
+        background-color: gary
+        }
+
+header {background-color:blue;
+        color: white,
+        font-size: 12px;
+        font-weight: 500;
+        font-style: normal;
+}
+
+div{
+    display: inline-block;
+    vertical-align: middle;
+}
+
+.manager{
+    background-color: white;
+}
+
+.intern{
+    background-color: white;
+}
+
+engineer{
+    background-color: white;
+}
+
+</style>
+
+        
+
+    </head>
+    <body>
+        
+    </body>
+    </html>
+    `
+}
